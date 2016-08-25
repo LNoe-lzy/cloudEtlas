065eb559c82f90c385e816e31fa893b7348d7ddd
LNoe-lzy <m15222505516@163.com>
2016-08-25 23:09:06 +0800

    修改前端逻辑。

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,26 +1,24 @@
 $(document).ready(function () {
     //图片展示
     $(function() {
-        $('.msg-con').each(function () {
+        $('.msg-con').click(function () {
             var that = $(this);
-            $(this).click(function () {
-                //获取传到弹出框的当前点击元素的各值
-                var text = that.find('p').html();
-                var imgSrc = that.find('div').find('img').attr('src');
-                var currentParent = that.parent();
-                var userName = currentParent.find('.msg-user').html();
-                var createTime = currentParent.find('.msg-time').html();
-                var userHead = currentParent.parent().find('.msg-head img').attr('src');
-                var imgModal = $('#imgModal');
-                imgModal.find('.modal-userhead img').attr({
-                    'src': userHead
-                });
-                imgModal.find('.msg-user').html(userName);
-                imgModal.find('.msg-time').html(createTime);
-                imgModal.find('.msg-info').html(text);
-                imgModal.find('.msg-img img').attr({
-                    'src': imgSrc
-                });
+            //获取传到弹出框的当前点击元素的各值
+            var text = that.find('p').html();
+            var imgSrc = that.find('div').find('img').attr('src');
+            var currentParent = that.parent();
+            var userName = currentParent.find('.msg-user').html();
+            var createTime = currentParent.find('.msg-time').html();
+            var userHead = currentParent.parent().find('.msg-head img').attr('src');
+            var imgModal = $('#imgModal');
+            imgModal.find('.modal-userhead img').attr({
+                'src': userHead
+            });
+            imgModal.find('.msg-user').html(userName);
+            imgModal.find('.msg-time').html(createTime);
+            imgModal.find('.msg-info').html(text);
+            imgModal.find('.msg-img img').attr({
+                'src': imgSrc
             });
         });
     });
@@ -40,31 +38,27 @@ $(document).ready(function () {
     });
     //删除ajax
     $(function () {
-        $('.delete').each(function () {
-            var that = $(this);
-            that.click(function () {
-                var imgPath = that.parent().find('.msg-img img').attr('src'),
-                    imgUser = that.parent().find('.msg-user').html();
-                $.ajax({
-                    type: 'get',
-                    dataType: 'json',
-                    url: '/delete',
-                    data: {
-                        imgPath: imgPath,
-                        imgUser: imgUser
-                    }
-                });
+        $('.delete').click(function () {
+            var that = $(this),
+                imgPath = that.parent().find('.msg-img img').attr('src'),
+                imgUser = that.parent().find('.msg-user').html();
+            $.ajax({
+                type: 'get',
+                dataType: 'json',
+                url: '/delete',
+                data: {
+                    imgPath: imgPath,
+                    imgUser: imgUser
+                }
             });
         });
     });
     //显示评论区
     //隐藏评论区
     $(function () {
-        $('.module-comment').each(function () {
+        $('.module-comment').click(function () {
             var that = $(this);
-            that.click(function () {
-                that.parent().parent().parent().find('.comment-text').fadeIn();
-            });
+            that.parent().parent().parent().find('.comment-text').fadeIn();
         });
         $('.btn-comment').click(function () {
             var commentTo = $(this).parent().find('.comment-user').html();
@@ -74,11 +68,9 @@ $(document).ready(function () {
             });
             commentArea.fadeIn();
         });
-        $('.comment-close').each(function () {
+        $('.comment-close').click(function () {
             var that = $(this);
-            that.click(function () {
-                that.parent().fadeOut();
-            });
+            that.parent().fadeOut();
         });
     });
     //用户头像上传
